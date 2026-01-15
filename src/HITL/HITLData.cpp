0dbcbc8007b5460136ca3e290238aa37cd93d296
dsrivas1 <145722611+dsrivas1@users.noreply.github.com>
2026-01-15 12:21:24 -0500

    added comments so astra would compile

--- src/HITL/HITLData.cpp
+++ src/HITL/HITLData.cpp
@@ -28,78 +28,78 @@ HITLData::HITLData(float _timestamp_s,
                   fix(_fix), fixqual(_fixqual), heading_deg(_heading_deg)
             {} //parameterized constructor
 
-HITLData parser(std::FILE *file){
-    if (file == NULL){
-                __throw_invalid_argument; //file not found, need better error logging system 
-            }
+// HITLData parser(std::FILE *file){
+//     if (file == NULL){
+//                 __throw_invalid_argument; //file not found, need better error logging system 
+//             }
 
-            // Get buffer instance
-            HITLData buffer;
+//             // Get buffer instance
+//             HITLData buffer;
 
-            // Parse CSV data
-            // Format: timestamp,ax,ay,az,gx,gy,gz,mx,my,mz,pressure,temp,lat,lon,alt,fix,fixqual,heading
+//             // Parse CSV data
+//             // Format: timestamp,ax,ay,az,gx,gy,gz,mx,my,mz,pressure,temp,lat,lon,alt,fix,fixqual,heading
 
-            while(stdin != out){ //do while loop until the end of the file 
+//             while(stdin != out){ //do while loop until the end of the file 
 
     
-            int itemsParsed = sscanf(file, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%d,%d,%lf",
-                                     &buffer.timestamp_s,
-                                     &buffer.ax,
-                                     &buffer.ay,
-                                     &buffer.az,
-                                     &buffer.data.gyro.x(),
-                                     &buffer.data.gyro.y(),
-                                     &buffer.data.gyro.z(),
-                                     &buffer.data.mag.x(),
-                                     &buffer.data.mag.y(),
-                                     &buffer.data.mag.z(),
-                                     &buffer.data.pressure,
-                                     &buffer.data.temperature,
-                                     &buffer.data.gps_lat,
-                                     &buffer.data.gps_lon,
-                                     &buffer.data.gps_alt,
-                                     (int *)&buffer.data.gps_fix,
-                                     &buffer.data.gps_fix_quality,
-                                     &buffer.data.gps_heading);
-            }
+//             int itemsParsed = sscanf(file, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%d,%d,%lf",
+//                                      &buffer.timestamp_s,
+//                                      &buffer.ax,
+//                                      &buffer.ay,
+//                                      &buffer.az,
+//                                      &buffer.data.gyro.x(),
+//                                      &buffer.data.gyro.y(),
+//                                      &buffer.data.gyro.z(),
+//                                      &buffer.data.mag.x(),
+//                                      &buffer.data.mag.y(),
+//                                      &buffer.data.mag.z(),
+//                                      &buffer.data.pressure,
+//                                      &buffer.data.temperature,
+//                                      &buffer.data.gps_lat,
+//                                      &buffer.data.gps_lon,
+//                                      &buffer.data.gps_alt,
+//                                      (int *)&buffer.data.gps_fix,
+//                                      &buffer.data.gps_fix_quality,
+//                                      &buffer.data.gps_heading);
+//             }
 
-            if (itemsParsed != 18)
-            {
-                LOGE("HITL: Parse error, got %d items (expected 18)", itemsParsed);
-                __throw_invalid_argument;
-            }
-
-
-            // Return timestamp
-            timestamp = buffer.timestamp_s;
-
-            return buffer;
-
-}
-
-uint16_t HITLData::encode(uint8_t *data, uint16_t sz){
-
-    uint16_t pos = 0;
-
-    if(sz < this->PAYLOAD_SIZE){
-        return 0; //error, size is too small 
-    }
-
-    if(this->type != 0x09){
-        return 0; //type is not HITLData
-    }
-
-    data[pos++] = this->type;
-    data[pos++] = (uint8_t)this->timestamp_s;
-    data[pos++] = (uint8_t)this->ax;
-    data[pos++] = (uint8_t)this->ay;
-    data[pos++] = (uint8_t)this->az; //this is defintely wrong, will get a lot of stuff "forgotten" because i'm casting a float 
-                                     // which is around 4 bytes into an 8 bit unsigned int (1 byte unsigned int)
+//             if (itemsParsed != 18)
+//             {
+//                 LOGE("HITL: Parse error, got %d items (expected 18)", itemsParsed);
+//                 __throw_invalid_argument;
+//             }
 
 
+//             // Return timestamp
+//             timestamp = buffer.timestamp_s;
+
+//             return buffer;
+
+// }
+
+// uint16_t HITLData::encode(uint8_t *data, uint16_t sz){
+
+//     uint16_t pos = 0;
+
+//     if(sz < this->PAYLOAD_SIZE){
+//         return 0; //error, size is too small 
+//     }
+
+//     if(this->type != 0x09){
+//         return 0; //type is not HITLData
+//     }
+
+//     data[pos++] = this->type;
+//     data[pos++] = (uint8_t)this->timestamp_s;
+//     data[pos++] = (uint8_t)this->ax;
+//     data[pos++] = (uint8_t)this->ay;
+//     data[pos++] = (uint8_t)this->az; //this is defintely wrong, will get a lot of stuff "forgotten" because i'm casting a float 
+//                                      // which is around 4 bytes into an 8 bit unsigned int (1 byte unsigned int)
 
 
-}
+
+
+// }
 
 
 
